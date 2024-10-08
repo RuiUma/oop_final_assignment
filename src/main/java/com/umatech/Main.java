72b17ef39382da85ecb9d96b4bcf0cd593f44f41
Eason Ma <theoneuma9@gmail.com>
2024-10-08 16:24:11 -0400

    config embedded tomcat and auto scan for servlets. Added Spring-like annotations.

--- src/main/java/com/umatech/Main.java
+++ src/main/java/com/umatech/Main.java
@@ -1,17 +1,11 @@
 package com.umatech;
+import com.umatech.TomcatApplication.AutoScan.TomcatApplication;
+import com.umatech.TomcatApplication.TomcatApplicationRunner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+@TomcatApplication
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) throws Exception {
+        TomcatApplicationRunner.run(Main.class, args);
     }
+
 }
